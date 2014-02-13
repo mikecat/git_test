831a26973880f5de2d9a1a6b681bd4bb4f1e0fc0
MikeCAT <mikecat@users.noreply.github.com>
2014-02-13 12:36:15 +0900

    added input check

--- add_main.c
+++ add_main.c
@@ -4,7 +4,10 @@ int add(int,int);
 
 int main(void) {
 	int a,b;
-	scanf("%d%d",&a,&b);
+	if(scanf("%d%d",&a,&b)!=2) {
+		puts("Invalid input!");
+		return 1;
+	}
 	printf("%d + %d = %d\n",a,b,add(a,b));
 	return 0;
 }
