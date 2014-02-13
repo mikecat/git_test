30f7238802036bc26a34a57bfb632be394014dfd
MikeCAT <mikecat@users.noreply.github.com>
2014-02-13 12:48:05 +0900

    added overflow check after addition

--- add_func.c
+++ add_func.c
@@ -7,3 +7,21 @@
 int add(int a,int b) {
 	return a+b;
 }
+
+/**
+ * ®”‚Ì‘«‚µZ‚ÌŒŸZ‚ğ‚µ‚Ü‚·B
+ * @param a ‘«‚³‚ê‚é”
+ * @param b ‘«‚·”
+ * @param sum a+b
+ * @return ³‚µ‚»‚¤‚È‚ç^AŠÔˆá‚Á‚Ä‚¢‚é‚È‚ç‹U
+ */
+int add_check(int a,int b,int sum) {
+	if(a+b!=sum || sum-a!=b || sum-b!=a)return 0;
+	if(b==0 && a!=sum)return 0;
+	if(a==0 && b!=sum)return 0;
+	if(b>0 && a>=sum)return 0;
+	if(b<0 && a<=sum)return 0;
+	if(a>0 && b>=sum)return 0;
+	if(a<0 && b<=sum)return 0;
+	return 1;
+}
--- add_main.c
+++ add_main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int add(int,int);
+int add_check(int,int,int);
 
 int main(void) {
-	int a,b;
+	int a,b,sum;
 	if(scanf("%d%d",&a,&b)!=2) {
 		puts("Invalid input!");
 		return 1;
+	}
+	sum = add(a,b);
+	if(!add_check(a,b,sum)) {
+		puts("Validation failed! (overflow?)");
+		return 2;
 	}
 	printf("%d + %d = %d\n",a,b,add(a,b));
 	return 0;
